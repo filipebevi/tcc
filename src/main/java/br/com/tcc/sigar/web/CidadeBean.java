d40f804260ed317b8e337660426b3fd8794f4251
Flipe <Flipe@192.168.25.4>
2017-04-08 19:14:21 -0300

    Comit das classes Cidade

---
+++ src/main/java/br/com/tcc/sigar/web/CidadeBean.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tcc.sigar.web;
+
+import br.com.tcc.sigar.cidade.Cidade;
+import br.com.tcc.sigar.cidade.CidadeRN;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Flipe
+ */
+@ManagedBean
+@RequestScoped
+public class CidadeBean {
+    
+    Cidade cidade = new Cidade();
+    List<Cidade> lista;
+ 
+    
+    public String novo(){
+        this.cidade = new Cidade();
+        return "cadastrarcidade";
+    }
+    
+    public String salvar(){
+        CidadeRN cidadeRN = new CidadeRN();
+        cidadeRN.salvar(this.cidade);
+        
+        return "cidade";
+    }
+
+    public List<Cidade> getLista() {
+       if(this.lista==null){
+            CidadeRN cidadeRN = new CidadeRN();
+            this.lista=cidadeRN.listar();
+       }
+        
+        return this.lista;
+    }
+    
+   
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+    
+    
+    
+}
